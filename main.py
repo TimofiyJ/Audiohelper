af8ecbdf4fefbe5418646a137dbbaedd5d10ad11
Tymofii Nasobko <44300490+TimofiyJ@users.noreply.github.com>
2024-06-20 13:15:07 +0300

    feat: add RAG

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 
     class GenerateAnswer(dspy.Signature):
         """With the provided context answer the question about person.
-        You have to be gentle and nice. Don't answer personal information such as password"""
+        You have to be gentle and nice"""
 
         context = dspy.InputField(
             desc="Information about person."
@@ -65,7 +65,7 @@ if __name__ == "__main__":
                 model_name=os.getenv("RETREIVAL_MODEL_NAME"),
             )
             self.retrieve = ChromadbRM(
-                "cancer",
+                "rag",
                 persist_directory=os.path.join("rag"),
                 embedding_function=self.huggingface_ef,
                 k=2,
@@ -108,13 +108,13 @@ if __name__ == "__main__":
     helper_agent = autogen.ConversableAgent(
         name="Helper_Agent",
         system_message="You are responsible for answering personal questions that have predefined answers. \
-            For example: Where are you? When you will be online?\
+            For example: Where are you? When you will be in town?\
                 Also You are responsible for assigning meetings, reminders, tasks given a request and data about the future plans \
             You need to ask for this information, such as time, place, title, summary if it is not provided.\
                 If at the end of the message you receive TERMINATE AGENT CONVERSATION 200 write TERMINATE",
         llm_config={"config_list": [config_list]},
         code_execution_config=False,
-        # max_consecutive_auto_reply=1,
+        max_consecutive_auto_reply=1,
         human_input_mode="NEVER",
     )
 
@@ -159,28 +159,28 @@ if __name__ == "__main__":
         print(f"Question: {question}")
         print(f"Predicted Answer: {pred.answer}")
 
-        return pred
+        return pred.answer
 
     autogen.register_function(
         create_event,
         caller=helper_agent,
         executor=manager_agent,
         name="calendar_create_tool",
-        description="Tool that helps create new event in calendar",
+        description="Tool that helps create new event in calendar. Use it when you have the word calendar",
     )
     autogen.register_function(
         get_events,
         caller=helper_agent,
         executor=manager_agent,
         name="calendar_read_tool",
-        description="Tool that helps get events from calendar",
+        description="Tool that helps get events from calendar. Use it when you have the word calendar",
     )
     autogen.register_function(
         get_answer,
         caller=helper_agent,
         executor=manager_agent,
         name="predefined_questions_tool",
-        description="Tool that helps get answers for personal predefined questions",
+        description="Tool that helps get answers for personal predefined questions such as When, What, Who, Why",
     )
 
     while 1:
